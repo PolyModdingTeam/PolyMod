6579fcca508491f51ea4ac0a36743b08a7e77413
IExploitableMan <pythonblack5000@gmail.com>
2025-01-10 17:40:53 +0300

    Add `name` property to the manifest (closes #26)

--- src/ModLoader.cs
+++ src/ModLoader.cs
@@ -24,18 +24,20 @@ namespace PolyMod
 		public class Mod
 		{
 			public record Dependency(string id, Version min, Version max, bool required = true);
-			public record Manifest(string id, Version version, string[] authors, Dependency[] dependencies);
+			public record Manifest(string id, string? name, Version version, string[] authors, Dependency[]? dependencies);
 			public record File(string name, byte[] bytes);
 			public enum Status { SUCCESS, ERROR };
 
+			public string? name;
 			public Version version;
 			public string[] authors;
-			public Dependency[] dependencies;
+			public Dependency[]? dependencies;
 			public Status status;
 			public List<File> files;
 
 			public Mod(Manifest manifest, Status status, List<File> files)
 			{
+				name = manifest.name ?? manifest.id;
 				version = manifest.version;
 				authors = manifest.authors;
 				dependencies = manifest.dependencies;
@@ -220,7 +222,7 @@ namespace PolyMod
 				}
 				else
 				{
-					Plugin.logger.LogError("Error on registering mod");
+					Plugin.logger.LogError("An invalid mod manifest was found or not found at all");
 				}
 			}
 
@@ -310,6 +312,7 @@ namespace PolyMod
 			GameManager.GetSpriteAtlasManager().cachedSprites.TryAdd("Heads", new());
 			Mod.Manifest polytopia = new(
 				"polytopia",
+				"The Battle of Polytopia",
 				new(VersionManager.SemanticVersion.ToString()),
 				Array.Empty<string>(),
 				Array.Empty<Mod.Dependency>()
