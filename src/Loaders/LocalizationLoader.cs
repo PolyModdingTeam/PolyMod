40c4746316049b98264e003809c6bb0fd1be4a44
unknown <gpluiy@gmail.com>
2025-02-07 15:39:59 +0100

    Improved custom skins load and closed #45

--- src/Loaders/LocalizationLoader.cs
+++ src/Loaders/LocalizationLoader.cs
@@ -2,11 +2,25 @@ using HarmonyLib;
 using I2.Loc;
 using Il2CppInterop.Runtime.InteropTypes.Arrays;
 using LibCpp2IL;
+using Polytopia.Data;
 
 namespace PolyMod.Loaders
 {
 	public static class LocalizationLoader
 	{
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(SelectTribePopup), nameof(SelectTribePopup.SetDescription))]
+		private static void SetDescription(SelectTribePopup __instance)
+		{
+			if ((int)__instance.SkinType >= Plugin.AUTOIDX_STARTS_FROM)
+			{
+				__instance.Description = Localization.Get(__instance.SkinType.GetLocalizationDescriptionKey()) + "\n\n" + Localization.GetSkinned(__instance.SkinType, __instance.tribeData.description2, new Il2CppSystem.Object[]
+				{
+					__instance.tribeName,
+					Localization.Get(__instance.startTechSid, Array.Empty<Il2CppSystem.Object>())
+				});
+			}
+		}
 		public static void BuildAndLoadLocalization(Dictionary<string, Dictionary<string, string>> localization)
 		{
 			foreach (var (key, data) in localization)
