e981846303837e6bbbf0910cbd1f6b7ba7470235
IExploitableMan <pythonblack5000@gmail.com>
2025-02-08 14:03:24 +0300

    Better polytopia version in props

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -9,19 +9,17 @@ using PolyMod.Managers;
 namespace PolyMod
 {
 	[BepInPlugin("com.polymod", "PolyMod", VERSION)]
-	public class Plugin : BepInEx.Unity.IL2CPP.BasePlugin
+	public partial class Plugin : BepInEx.Unity.IL2CPP.BasePlugin
 	{
 		internal record PolyConfig(
 			bool debug = false
 		);
 
-		internal const string VERSION = Props.VERSION;
 		internal const int AUTOIDX_STARTS_FROM = 1000;
 		public static readonly string BASE_PATH = Path.Combine(BepInEx.Paths.BepInExRootPath, "..");
 		public static readonly string MODS_PATH = Path.Combine(BASE_PATH, "Mods");
 		public static readonly string DUMPED_DATA_PATH = Path.Combine(BASE_PATH, "DumpedData");
 		internal static readonly string CONFIG_PATH = Path.Combine(BASE_PATH, "PolyMod.json");
-		internal static readonly Il2CppSystem.Version POLYTOPIA_VERSION = new(2, 12, 0);
 
 #pragma warning disable CS8618
 		internal static PolyConfig config;
