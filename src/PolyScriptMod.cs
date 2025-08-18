bba12511f04760bf97c384d2aff24b92f6bb3ff1
Jouke van Dam <jouke@tayami.net>
2025-08-18 17:04:51 +0200

    Use System.Text.json instead of newtonsoft

--- src/PolyScriptMod.cs
+++ src/PolyScriptMod.cs
@@ -1,3 +1,4 @@
+using BepInEx.Logging;
 using Newtonsoft.Json.Linq;
 using PolyMod.Managers;
 
@@ -5,7 +6,7 @@ namespace PolyMod;
 
 public abstract class PolyScriptModBase
 {
-    internal abstract void Initialize(string name);
+    internal abstract void Initialize(string name, ManualLogSource logger);
     public abstract void Load();
     public abstract void UnLoad();
     internal PolyScriptModBase()
@@ -14,16 +15,17 @@ public abstract class PolyScriptModBase
 }
 public abstract class PolyScriptMod<TConfig, TExposedConfig> : PolyScriptModBase where TConfig : class where TExposedConfig : class
 {
-    internal override void Initialize(string name)
+    internal override void Initialize(string name, ManualLogSource logger)
     {
         ModName = name;
         Config = new Config<TConfig>(name, Config<TConfig>.ConfigTypes.PerMod);
         ExposedConfig = new Config<TExposedConfig>(name, Config<TExposedConfig>.ConfigTypes.Exposed);
+        Logger = logger;
     }
     public string ModName { get; private set; }
     protected Config<TConfig> Config { get; private set; } = null!;
     protected Config<TExposedConfig> ExposedConfig { get; private set; } = null!;
-    protected virtual JObject DefaultConfig => new JObject();
+    protected ManualLogSource Logger { get; private set; } = null!;
 }
 
 public abstract class PolyScriptMod : PolyScriptMod<JObject, JObject>
