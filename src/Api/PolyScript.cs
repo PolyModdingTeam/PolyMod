02c4d379ef3fc0232cdb03ed641626c6d7486b5a
IExploitableMan <pythonblack5000@gmail.com>
2025-12-30 11:35:01 +0300

    Add `GetFile` API to `PolyScript`

--- src/Api/PolyScript.cs
+++ src/Api/PolyScript.cs
@@ -1,29 +1,47 @@
 using BepInEx.Logging;
 using Newtonsoft.Json.Linq;
-using PolyMod.Managers;
 
 namespace PolyMod.Api;
 
 public abstract class PolyScriptBase
 {
-    internal abstract void Initialize(string name, ManualLogSource logger);
+    /// <summary>
+    /// Initializes the PolyScript with the given mod and logger.
+    /// </summary>
+    /// <param name="mod">The mod instance.</param>
+    /// <param name="logger">The logger instance.</param>
+    internal abstract void Initialize(Mod mod, ManualLogSource logger);
+    
+    /// <summary>>
+    /// Called when the mod is loaded.
+    /// </summary>
     public abstract void Load();
-    public abstract void UnLoad();
+
+    /// <summary>
+    /// Called when the mod is unloaded.
+    /// </summary>
+    public abstract void Unload();
+
     internal PolyScriptBase()
     {
     }
 }
 public abstract class PolyScript<TConfig, TExposedConfig> : PolyScriptBase where TConfig : class where TExposedConfig : class
 {
-    internal override void Initialize(string name, ManualLogSource logger)
+    internal override void Initialize(Mod mod, ManualLogSource logger)
     {
-        ModName = name;
-        Config = new Config<TConfig>(name, Config<TConfig>.ConfigTypes.PerMod);
-        ExposedConfig = new Config<TExposedConfig>(name, Config<TExposedConfig>.ConfigTypes.Exposed);
+        Mod = mod;
+        Config = new Config<TConfig>(mod.id, Config<TConfig>.ConfigTypes.PerMod);
+        ExposedConfig = new Config<TExposedConfig>(mod.id, Config<TExposedConfig>.ConfigTypes.Exposed);
         Logger = logger;
     }
 
-    public string ModName { get; private set; } = null!;
+    internal byte[]? GetFile(string fileName)
+    {
+        return Registry.mods[Mod.id].files.FirstOrDefault(f => f.name == fileName)?.bytes;
+    }
+
+    public Mod Mod { get; private set; } = null!;
     protected Config<TConfig> Config { get; private set; } = null!;
     protected Config<TExposedConfig> ExposedConfig { get; private set; } = null!;
     protected ManualLogSource Logger { get; private set; } = null!;
